1c287ccedcbcc7229f19162ce62588782dcd822c
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2026-03-30 22:52:23 +0000

    arreglado responsive, foto portafolio añadida, promps correctamente enlazados

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
     {
       id: "1",
       name: "Portafolio",
-      url: "",
+      url: "https://github.com/Contredu/portafolio-carlos",
+      imagen: "./src/assets/portafolio carlos.png",
       description: "Les presento mi portafolio mi primer proyecto propio hecho desde cero, aquí he utilizado React y Tailwind CSS, demostrando mis habilidades en desarrollo web."
     }
   ]
@@ -21,38 +22,45 @@ function App() {
   return (
     <>
       <div className="bg-linear-to-r from-blue-950 via-blue-600
-     to-fuchsia-700 text-center p-5">
+            to-fuchsia-700 text-center p-5">
 
-        <div className="flex flex-col border border-black rounded-lg shadow-lg/50 shadow-black text-white">
+        <div className="border border-black rounded-lg shadow-lg/50 shadow-black text-white">
           {/* Header */}
-          <div className="flex-row flex-wrap justify-between items-center p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 justify-around items-center p-4">
 
             {/* Side left */}
-            <div className="flex-1 my-4">
+            <div className="my-4 p-4">
               <h2 className="font-serif text-4xl">Conéctate conmigo</h2>
-              <section className="flex justify-center items-center my-6 gap-10">
+              <section className="flex flex-wrap justify-center items-center my-6 gap-10">
                 <a className="size-20 rounded-full bg-gray-400 shadow-black/80 shadow-lg"></a>
 
                 <a className=" size-20 rounded-full bg-gray-400 shadow-black/80 shadow-lg"></a>
 
                 <a className=" size-20 rounded-full bg-gray-400 shadow-black/80 shadow-lg"></a>
 
+                <a className=" size-20 rounded-full bg-gray-400 shadow-black/80 shadow-lg"></a>
+
+
               </section>
             </div>
 
             {/* Side rigth */}
-            <div className="flex-1 my-4 mx-4 border rounded-xl p-4 bg-white/10 shadow-md shadow-white/20">
+            <div className="my-4 mx-4 border rounded-xl p-4 bg-white/10 shadow-md shadow-white/20">
 
               {/* Card presentation */}
-              <div className="flex justify-center items-center">
-                <div className="flex-auto p-4">
+              <div className="flex flex-wrap justify-center items-center gap-4">
+                <div className="p-4">
                   <img src="./src/assets/foto-perfil.png" alt="" className="object-cover object-center size-30 rounded-full shadow-lg shadow-gray-700 hover:scale-105 transition-transform" />
                 </div>
-                <div className="flex-auto w-100">
+                <div className="w-100">
                   <h2 className="text-2xl">QUIEN SOY Y MI PASION</h2>
-                  <p className="text-black">bsafdvfbasmjsakdb hvabsdivas vbashdvasvdkhvbas jhsafhsavdvashj bashbdhfvadsfvasd kjsbcad
-                    aswdfiadsgfuega asdfuaudfgaus asdufbasuidbfa basdufuasdfbasdu uadsfubskkfoah fuascbsa
-                    dwasufasudigfagu
+                  <p className="text-black">Desarrollador Full Stack con experiencia en JavaScript, React, Bootstrap y
+                    Python, enfocado en crear soluciones eficientes, seguras y orientadas al usuario.
+                    Mi camino comenzó desde el emprendimiento: la necesidad de construir mi
+                    propio ecommerce despertó mi pasión por la programación. Hoy combino
+                    visión de negocio con mentalidad técnica para crear proyectos escalables y con
+                    impacto real. Destaco por mi curiosidad constante, pensamiento analítico y el
+                    deseo de ayudar a otros a entender y aprovechar el mundo tecnológico.
                   </p>
                 </div>
               </div>
@@ -63,7 +71,7 @@ function App() {
 
           {/* Body */}
           <h1 className="text-5xl shadow-lg shadow-black/30 my-10 p-4">Mis proyectos</h1>
-          <div className="flex flex-wrap justify-between items-center p-4">
+          <div className="grid grid-cols-1 p-4 sm:grid-cols-3 gap-4">
 
             {/* Projects Whit AI */}
             <div className="m-2">
@@ -117,17 +125,18 @@ function App() {
               {OwnProjects.length === 0 ? (
                 <h1 className="text-2xl my-10">No hay proyectos aún.</h1>
               ) : (OwnProjects.map((project) =>
-                
-                  <div className="m-5 bg-white/20 border-2 border-black rounded-lg" key={project.id}>
-                    <img src="./src/assets/Reno.png" className="p-2 justify-center size-fit object-cover" />
-                    <div className="my-2">
-                      <h3 className="text-2xl">{project.name}</h3>
-                      <p className="">{project.description}</p>
-                    </div>
-                    <a className="my-8 py-2 w-[50%] border-2 border-black" href={project.repositoryLink} target="_blank" rel="noopener noreferrer">
+
+                <div className="my-2 bg-white/20 border-2 border-black rounded-lg" key={project.id}>
+                  <img src={project.imagen} className="p-2 justify-center size-fit object-cover" />
+                  <div className="flex flex-col my-2">
+                    <h3 className="text-2xl mb-2 text-black font-bold text-shadow-lg text-shadow-gray-600">{project.name}</h3>
+                    <p className="">{project.description}</p>
+                    <a className="m-4 py-2 border-2 border-black rounded-2xl bg-black text-white hover:bg-gray-900 transition duration-150 hover:scale-105" href={project.url} target="_blank" rel="noopener noreferrer">
                       Enlace a repositorio
                     </a>
                   </div>
+
+                </div>
               )
               )}
 
