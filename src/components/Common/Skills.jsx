9e322650162fc5c9a49eefbd663013874c48566c
Carlos Enrique Contreras Duque <carlos.contredu@gmail.com>
2026-04-07 11:10:16 +0000

    logotipos de lenguajes agregados en sprite.svg

--- src/components/Common/Skills.jsx
+++ src/components/Common/Skills.jsx
@@ -4,14 +4,53 @@ export const MySkills = () => {
         <>
             <div className="flex flex-wrap justify-center items-start my-6 gap-8">
                 {/* Aquí puedes agregar tus habilidades */}
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">JavaScript</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">React</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">Node.js</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">CSS</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">HTML</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">Python</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">TailwindCSS</span>
-                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">Flask</span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="bg-white text-[#F7DF1E] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-javascript"/>
+                        </svg>
+                    JavaScript
+                </span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                                        <svg className="text-[#61DAFB] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-react"/>
+                        </svg>React</span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="text-[#5FA04E] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-nodejs"/>
+                    </svg>
+                    Node.js
+                </span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="bg-white text-[#663399] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-css"/>
+                    </svg>
+                    CSS
+                </span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="text-[#E34F26] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-html"/>
+                    </svg>
+                    HTML
+                </span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+
+                        <img src="./src/assets/SVG Redes sociales/python-logo-only.svg" alt="Python" className="object-fit size-6 rounded-full inline-block mr-2"/>
+
+                    Python
+                </span>
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="text-[#06B6D4] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-tailwindcss"/>
+                    </svg>
+                    TailwindCSS
+                </span>
+
+                <span className="bg-cyan-200 text-black font-semibold px-4 py-2 rounded-lg shadow-md shadow-cyan-500">
+                    <svg className="text-[#3BABC3] size-6 rounded-full inline-block mr-2">
+                        <use href="./src/assets/SVG Redes sociales/sprite.svg#icon-flask"/>
+                    </svg>
+                    Flask
+                </span>
                 {/* Agrega más habilidades según sea necesario */}
             </div>
         </>
